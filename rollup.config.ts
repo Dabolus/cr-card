720fc2b5e3b3921f592580c2448d0045743f1c31
Giorgio Garasto <giorgio@garasto.it>
2019-01-28 08:49:03 +0100

    chore: Configure rollup

---
+++ rollup.config.ts
@@ -0,0 +1,66 @@
+// tslint:disable:object-literal-sort-keys
+import { readdirSync } from 'fs';
+import minifyHtml from 'rollup-plugin-minify-html-literals';
+import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
+
+const prod = process.env.NODE_ENV === 'production';
+
+const inputs = readdirSync('src')
+  .filter((file) => !file.endsWith('.d.ts'))
+  .map((file) => file.slice(0, -3));
+
+const getConfig = (input, minify) => ({
+  input: `src/${input}.ts`,
+  output: [{
+    file: `${input}${minify ? '.min' : ''}.js`,
+    format: 'es',
+    sourcemap: !prod,
+  }, {
+    file: `${input}.iife${minify ? '.min' : ''}.js`,
+    format: 'iife',
+    name: `dabolus.${input.replace(/-([a-z])/g, ([, l]) => l.toUpperCase())}`,
+    sourcemap: !prod,
+  }],
+  plugins: [
+    minifyHtml({
+      options: {
+        shouldMinify: (template) => template.parts[0].text.startsWith('<!-- html -->'),
+        minifyOptions: {
+          minifyCSS: true,
+          minifyJS: true,
+          collapseWhitespace: true,
+          collapseBooleanAttributes: true,
+          collapseInlineTagWhitespace: true,
+          removeOptionalTags: true,
+          removeTagWhitespace: true,
+          sortAttributes: true,
+          sortClassName: true,
+          removeRedundantAttributes: true,
+        }
+      }
+    }),
+    typescript({
+      tsconfigOverride: {
+        compilerOptions: {
+          declaration: prod,
+          sourceMap: !prod,
+        },
+      },
+    }),
+    ...minify ? [
+      terser({
+        mangle: {
+          properties: {
+            regex: /^_/,
+          },
+        },
+      }),
+    ] : [],
+  ],
+  // Make all dependencies external
+  external: () => true,
+});
+
+export default inputs.reduce((configs, input) =>
+  [...configs, getConfig(input, false), ...prod ? [getConfig(input, true)] : []], []);
