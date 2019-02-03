9a0ba60671be618c342575e2dda55fb3b36bf988
Giorgio Garasto <giorgio@garasto.it>
2019-02-03 23:35:42 +0100

    feature: Setup cr-card-property

---
+++ src/cr-card-property.ts
@@ -0,0 +1,10 @@
+/**
+ * `cr-card-property`
+ * This element represents the property of a Clash Royale card.
+ *
+ * It is meant to be used only inside a `cr-card` element, so it is quite useless by itself.
+ *
+ * @customElement
+ */
+export class CRCardProperty extends HTMLElement {
+}
